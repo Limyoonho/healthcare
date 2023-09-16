bcaaef856b2168cf83c79c6b39a0cbc86d27668c
aneunwook <dmsdnr0720@naver.com>
2023-09-16 21:42:38 +0900

    main창 테스트 본

---
+++ src/main/java/project/healthcare/Controller/test.java
@@ -0,0 +1,26 @@
+package project.healthcare.Controller;
+
+import project.healthcare.Entity.TestTable;
+import project.healthcare.Service.TestTableService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class test {
+
+    @Autowired
+    TestTableService testTableService; // command left mouse click
+
+    @RequestMapping("/home")
+    public String home(Model model){
+        List<TestTable> list = testTableService.findAll();
+        model.addAttribute("list",list);
+        model.addAttribute("hi","hiEveryOne");
+    return "main";
+    }
+
+}
