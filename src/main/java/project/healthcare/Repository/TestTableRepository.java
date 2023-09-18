792068b5d4f97f0733ddf21bb4c2908d18ec0129
Limyoonho <102271663+Limyoonho@users.noreply.github.com>
2023-09-18 19:39:00 +0900

    Delete src/main/java/project/healthcare/Repository/TestTableRepository.java

--- src/main/java/project/healthcare/Repository/TestTableRepository.java
+++
@@ -1,9 +0,0 @@
-package project.healthcare.Repository;
-
-import project.healthcare.Entity.TestTable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TestTableRepository extends JpaRepository<TestTable, Long> {
-}
