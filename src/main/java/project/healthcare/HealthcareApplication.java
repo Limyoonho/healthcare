7684642d734e1da7a2913ac4e4048026a12dde9d
aneunwook <dmsdnr0720@naver.com>
2023-09-18 21:09:51 +0900

    외부라이브러리(jsoup) 추가 & 크롤링 확인 하는 테스트 (성공)

--- src/main/java/project/healthcare/HealthcareApplication.java
+++ src/main/java/project/healthcare/HealthcareApplication.java
@@ -3,6 +3,8 @@ package project.healthcare;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class HealthcareApplication {
 
