85e9741ed87776277b39a67e66afd5a6f7559243
jhkim <jhkim@daegu.ac.kr>
2023-09-24 20:45:29 +0900

    메인웹페이지 설문조사배너, 설문조사페이지양식추가, 설문조사 제출 후 완료페이지, 설문조사 기록 db테이블 추가

---
+++ src/main/java/project/healthcare/Service/SurveyTableService.java
@@ -0,0 +1,21 @@
+package project.healthcare.Service;
+//
+//import project.healthcare.Entity.SurveyEntity;
+//import project.healthcare.Repository.SurveyTableRepository;
+//import jakarta.transaction.Transactional;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
+//@Service
+//@Transactional
+//public class SurveyTableService {
+//
+//    @Autowired
+//    private SurveyTableRepository surveyTableRepository;
+////
+////    public List<SurveyEntity> findAll(){
+////        return SurveyTableRepository.findAll();
+////    }
+//}
