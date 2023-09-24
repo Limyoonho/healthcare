734d934b62ef15c5cf5d6c034dc850a4dbf04939
qta3426 <qta3426@naver.com>
2023-09-25 01:13:57 +0900

    비밀번호 암호화 기능 (추후 sercurity 관련 추가 예정)

---
+++ src/main/java/project/healthcare/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package project.healthcare.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Slf4j
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
