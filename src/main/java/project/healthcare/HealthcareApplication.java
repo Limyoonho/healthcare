d943ae815ee97ee3e9a51fa1b9f7307497a4b609
qta3426 <58142826+qta3426@users.noreply.github.com>
2023-09-25 20:22:26 +0900

    Update HealthcareApplication.java
    import 수정

--- src/main/java/project/healthcare/HealthcareApplication.java
+++ src/main/java/project/healthcare/HealthcareApplication.java
@@ -2,9 +2,9 @@ package project.healthcare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class HealthcareApplication {
 
 	public static void main(String[] args) {
