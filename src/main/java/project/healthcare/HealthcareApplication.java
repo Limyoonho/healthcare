f85c29a7adca15eea163c729e7938f35bc1cf865
qta3426 <58142826+qta3426@users.noreply.github.com>
2023-09-25 20:11:19 +0900

    Update HealthcareApplication.java
    spring security로 뜨는 기본 로그인 화면 제거

--- src/main/java/project/healthcare/HealthcareApplication.java
+++ src/main/java/project/healthcare/HealthcareApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class HealthcareApplication {
 
 	public static void main(String[] args) {
