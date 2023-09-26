1d77037fb128623a183f069d1cbcb71203ec2bf8
qta3426 <qta3426@naver.com>
2023-09-27 00:27:50 +0900

    security 내용 추가

--- src/main/java/project/healthcare/config/SecurityConfig.java
+++ src/main/java/project/healthcare/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package project.healthcare.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,10 +9,46 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import project.healthcare.component.CustomAuthenticationSuccessHandler;
+import project.healthcare.component.CustomAuthenticationFailureHandler;
 
 @Slf4j
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    @Autowired
+    private CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
+    @Autowired
+    private CustomAuthenticationFailureHandler customAuthenticationFailureHandler;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authorizeRequests)->
+                authorizeRequests
+                .requestMatchers("/").permitAll()
+                .requestMatchers("/login").permitAll()
+                .requestMatchers("/join").permitAll()
+                .requestMatchers("/css/**").permitAll()
+                .requestMatchers("/img/**").permitAll()
+                .anyRequest().authenticated());
+        http.formLogin((formLogin)->
+                formLogin
+                .loginPage("/login")
+                .loginProcessingUrl("/authenticate")
+                .successHandler(customAuthenticationSuccessHandler)
+                .failureHandler(customAuthenticationFailureHandler)
+                .permitAll());
+        http.logout((logoutConfig)->
+                logoutConfig
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true));
+        http.exceptionHandling((exceptionHandling)->
+                exceptionHandling
+                .accessDeniedPage("/denied"));
+        return http.build();
+    }
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
