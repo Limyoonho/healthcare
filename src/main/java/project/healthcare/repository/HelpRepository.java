c84aff705818c84642790e8495bc83cc1d6d6874
qta3426 <qta3426@naver.com>
2023-10-09 19:01:15 +0900

    경로 수정

--- src/main/java/project/healthcare/repository/HelpRepository.java
+++ src/main/java/project/healthcare/repository/HelpRepository.java
@@ -1,10 +1,10 @@
-package project.healthcare.repository;
+package project.healthcare.Repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import project.healthcare.entity.Help;
+import project.healthcare.Entity.Help;
 
 @Repository
 public interface HelpRepository extends JpaRepository<Help,Integer> {
--- src/main/java/project/healthcare/service/HelpService.java
+++ src/main/java/project/healthcare/service/HelpService.java
@@ -1,11 +1,11 @@
-package project.healthcare.service;
+package project.healthcare.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import project.healthcare.entity.Help;
-import project.healthcare.repository.HelpRepository;
+import project.healthcare.Entity.Help;
+import project.healthcare.Repository.HelpRepository;
 
 import java.sql.Date;
 import java.util.List;
