084371f79dfe012f9bb6964a7064e4f2d01ed192
qta3426 <qta3426@naver.com>
2023-10-10 21:58:31 +0900

    로그아웃 후 로그인 페이지가 아닌 메인으로 redirect

--- src/main/java/project/healthcare/config/SecurityConfig.java
+++ src/main/java/project/healthcare/config/SecurityConfig.java
@@ -5,7 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.domain.AbstractAuditable_;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -47,7 +49,7 @@ public class SecurityConfig {
         http
                 .logout(logout -> logout
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/login")
+                .logoutSuccessUrl("/")
                 .invalidateHttpSession(true)
                 );
         http
