8de998d5f533297b87e3076434d5d54d222e609c
qta3426 <qta3426@naver.com>
2023-10-10 15:13:56 +0900

    회원가입 후, 로그인 페이지로 이동 안하거나 회원가입이 안되었는데 login 페이지로 redirect 되는 문제 해결

--- src/main/java/project/healthcare/controller/UserController.java
+++ src/main/java/project/healthcare/controller/UserController.java
@@ -1,18 +1,27 @@
 package project.healthcare.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import project.healthcare.dto.UserDTO;
 import project.healthcare.Service.UserService;
 
-@RestController
+import java.net.URI;
+
+
+@Controller
 @AllArgsConstructor
 public class UserController {
 
     private final UserService userService;
     @PostMapping("/user/save")
     public String saveUser(UserDTO userDTO) {
-        return userService.saveUser(userDTO);
+        userService.saveUser(userDTO);
+        return "redirect:/login";
     }
 }
